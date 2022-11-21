4da80ca179c3a704463d947487d188165eca48c8
unknown <ntorilarry@gmail.com>
2022-11-21 09:58:52 +0000

    dev-larry

--- src/lib/presentation/components/profile.js
+++ src/lib/presentation/components/profile.js
@@ -1,25 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../pages/auth/auth";
+import Chesslogo from "../../core/resources/images/logowhite.png"
+import toast from "react-hot-toast";
 
 function Profile() {
-  const navigate = useNavigate()
-  const {account, ethBal, logout} = useAuth();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const { account, ethBal, logout } = useAuth();
 
   const onDisconnect = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     localStorage.removeItem("userAuthObjectModel");
+    toast.success("Logout Successful");
     logout();
     navigate("/auth/login", { replace: true });
   };
-  
+
   return (
-    <div className="flex my-auto py-auto">
-      <p className="bg-[#438FFE] px-2 py-2 text-white">{ethBal} ETH</p>
-      <p className="bg-white px-2 py-2 text-black ">{account}</p>
-      <button onClick={(e)=> onDisconnect(e)} className="text-[#438FFE] font-bold px-2 py-2 text-white">Logout</button>
+    <div>
+      <ul className="flex items-center hidden space-x-8 lg:flex">
+        <li className="bg-[#438FFE] py-2 px-2 font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400">
+          {ethBal} ETH
+        </li>
+        <li className="bg-white font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+          {account}
+        </li>
+
+        <li>
+          <button
+            onClick={(e) => onDisconnect(e)}
+            className="bg-[#ED1B24] inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+      <div class="lg:hidden">
+        <button
+          aria-label="Open Menu"
+          title="Open Menu"
+          class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+          onClick={() => setIsMenuOpen(true)}
+        >
+          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+            />
+            <path
+              fill="currentColor"
+              d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+            />
+            <path
+              fill="currentColor"
+              d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+            />
+          </svg>
+        </button>
+        {isMenuOpen && (
+          <div class="absolute top-0 left-0 w-full">
+            <div class="p-5 bg-white border rounded shadow-sm">
+              <div class="flex items-center justify-between mb-4">
+                <div>
+                  <a
+                    href="/"
+                    aria-label="Company"
+                    title="Company"
+                    class="inline-flex items-center"
+                  >
+                    <img className="" src={Chesslogo} alt=""/>
+                   
+                  </a>
+                </div>
+                <div>
+                  <button
+                    aria-label="Close Menu"
+                    title="Close Menu"
+                    class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <nav>
+                <ul class="space-y-4">
+                  <li class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+                  {ethBal} ETH
+                  </li>
+                  <li class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+                  {account}
+                  </li>
+                  <li>
+                    <button
+                      onClick={(e) => onDisconnect(e)}
+                      class=" bg-[#ED1B24] inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-    
   );
 }
 
